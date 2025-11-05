41b44cb1bb622073de25d59d4543796a51dc1e5d
Reina.Yamaguchi@partner.kaneka.co.jp <Reina.Yamaguchi@partner.kaneka.co.jp>
2025-11-05 05:43:01 +0000

    test

--- test_databricks/models/canvas/silver_test.sql
+++ test_databricks/models/canvas/silver_test.sql
@@ -1,20 +1,30 @@
+{{ config(
+  materialized='table',
+  catalog='dev_first_usecase',
+  schema='silver',
+  alias='silver_dbt_test'
+) }}
+
+
+
 WITH bronze_trn_sap AS (
+  SELECT * FROM {{ source('dev_first_usecase', 'bronze_trn_sap') }}
+),
+silver_test_sql AS (
   SELECT
-    *
-  FROM {{ source('dev_first_usecase', 'bronze_trn_sap') }}
-), silver_test_sql AS (
-  SELECT
-    record_month,
-    plan_type,
-    plan_type_name,
-    fiscal_year,
-    period,
-    cost_center,
-    cost_center_name,
-    budget_amount,
-    actual_amount
+  record_month,
+  plan_type,
+  plan_type_name,
+  fiscal_year,
+  period,
+  cost_center,
+  cost_center_name,
+  budget_amount,
+  actual_amount
   FROM bronze_trn_sap
 )
-SELECT
-  *
-FROM silver_test_sql
+
+
+
+SELECT * FROM silver_test_sql;
+ 
