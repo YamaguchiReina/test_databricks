efe372c7c257eb28df3c08ef0d9e4f2d03d99dd3
Reina Yamaguchi <Reina.Yamaguchi@partner.kaneka.co.jp>
2025-11-12 00:56:36 +0000

    test

--- test_databricks/models/silver/create_test.sql
+++ test_databricks/models/silver/create_test.sql
@@ -1,26 +1,18 @@
-{{ config(
-  materialized='table',
-  catalog='dev_first_usecase',
-  schema='silver',
-  alias='silver_dbt_sap'
-) }}
+{{
+    config(
+        materialized="table",
+        catalog="dev_first_usecase",
+        schema="silver",
+        alias="silver_dbt_account",
+    )
+}}
 
-WITH bronze_mst_account AS (
-  SELECT
-    ID,
-    S4_SAPCode1
-  FROM {{ source('dev_first_usecase', 'bronze_mst_account') }}
-), filter AS (
-  SELECT
-    *
-  FROM bronze_mst_account
-  WHERE
-    NOT S4_SAPCode1 IS NULL
-), untitled_sql AS (
-  SELECT
-    *
-  FROM filter
-)
-SELECT
-  *
-FROM untitled_sql
+with
+    bronze_mst_account as (
+        select id, s4_sapcode1
+        from {{ source("dev_first_usecase", "bronze_mst_account") }}
+    ),
+    filter as (select * from bronze_mst_account where not s4_sapcode1 is null),
+    untitled_sql as (select * from filter)
+select *
+from untitled_sql
